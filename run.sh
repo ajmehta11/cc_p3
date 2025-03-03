3be31b7416c0d3f29067e019ebb3e1eb01028b40
aryanistrying <aryanistrying@gmail.com>
2025-03-02 23:10:26 -0500

    new

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ spark-submit \
     --conf spark.sql.adaptive.enabled=true \
     --conf spark.sql.autoBroadcastJoinThreshold=10485760 \
     --conf spark.driver.memory=6g \
-    --num-executors=9 \
+    --num-executors=5 \
     --executor-memory=9g \
-    --executor-cores=2 \
-    --conf spark.default.parallelism=54 \
+    --executor-cores=7 \
+    --conf spark.default.parallelism=70 \
     --conf spark.storage.memoryFraction=0.6 \
     --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph-Topics wasbs:///pagerank-output wasbs:///recs-output
